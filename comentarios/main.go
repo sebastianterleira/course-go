9bde0d8e7644ea4c3752f8f6a0d21925d906155b
sebastianterleira <seb.terleira1204@gmail.com>
2023-01-27 12:05:38 -0500

    Comentarios y Documentacion con Go

---
+++ comentarios/main.go
@@ -0,0 +1,15 @@
+// package main es el paquete principal
+// que me permite ejecutar mi aplicacion
+package main
+
+// test es una variable de prueba
+var Test = 123
+
+// pi es una contante que vale 2.33
+const Pi = 2.33
+
+// comentario en una linea
+
+/* func main() {
+
+} */ 
